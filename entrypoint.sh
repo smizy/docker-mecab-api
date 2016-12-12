0a213f8f01e41f73a5e94d15c66eb394e195930a
smizy <shimizu@octetcode.com>
2016-12-12 18:15:10 +0900

    add microsecond response time to logformat

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ set -eo pipefail
 
 if [ "$1" == "gunicorn" ]; then
     shift
-    exec su-exec mecab gunicorn --access-logfile -  "$@"
+    exec su-exec mecab gunicorn \
+        --access-logformat '%(h)s %(l)s %(u)s %(t)s %(D)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' \
+        --access-logfile -  \
+        "$@"
 fi
 
 exec "$@"
